248099bd21dde55f7b95220ff8e99015c1f9bcf1
tomkf <thomaskeithferris@gmail.com>
2020-01-21 10:03:33 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Register from './components/auth/Register';
 import Welcome from './components/auth/Welcome';
 import LogIn from './components/auth/LogIn';
 import ForgotPassword from './components/auth/ForgotPassword';
+import ForgotPasswordSubmit from './components/auth/ForgotPasswordSubmit';
+
 
 class App extends Component {
   state = {
@@ -42,6 +44,8 @@ setAuthUser = user => {
               <Route exact path="/welcome" render = {(props) => <Welcome {...props} auth={authProps} />} />
               <Route exact path="/login" render = {(props) => <LogIn {...props} auth={authProps} />} />
               <Route exact path="/forgotpassword" render={(props) => <ForgotPassword {...props} auth={authProps} /> } />
+              <Route exact path="/forgotpasswordsubmit" render={(props) => <ForgotPasswordSubmit {...props} auth={authProps} /> } />
+
             </Switch>
           </div>
         </Router>
