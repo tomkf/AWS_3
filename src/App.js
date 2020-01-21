0d4302f8b387bc4745b3699afadd342162852d32
tomkf <thomaskeithferris@gmail.com>
2020-01-21 10:10:17 -0800

    change password component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Welcome from './components/auth/Welcome';
 import LogIn from './components/auth/LogIn';
 import ForgotPassword from './components/auth/ForgotPassword';
 import ForgotPasswordSubmit from './components/auth/ForgotPasswordSubmit';
+import ChangePasswordConfirmation from './components/auth/ChangePasswordConfirmation'
 
 
 class App extends Component {
@@ -45,7 +46,7 @@ setAuthUser = user => {
               <Route exact path="/login" render = {(props) => <LogIn {...props} auth={authProps} />} />
               <Route exact path="/forgotpassword" render={(props) => <ForgotPassword {...props} auth={authProps} /> } />
               <Route exact path="/forgotpasswordsubmit" render={(props) => <ForgotPasswordSubmit {...props} auth={authProps} /> } />
-
+              <Route exact path="/changepassword" render = {(props) => <ChangePasswordConfirmation {...props} auth={authProps} />} />
             </Switch>
           </div>
         </Router>
